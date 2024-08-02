9af9836f47da6ba922e65b7416cbc39a97c92313
lvcky-gg <matadorcrab@gmail.com>
2024-08-02 16:09:50 -0400

    feat: init

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -13,4 +13,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
 
+  get imagePath() {
+    return `assets/users/${this.selectedUser.avatar}`;
+  }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }
